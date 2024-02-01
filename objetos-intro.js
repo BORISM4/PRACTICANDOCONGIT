dcb18f57a1346de613e25f9e059b4c22e39b9fdf
BORISM4 <borismartinez29o@gmail.com>
2024-01-31 18:16:58 -0600

    hola papu

---
+++ objetos-intro.js
@@ -0,0 +1,3 @@
+const personaUno = ["PACHECO MENDOZA", 23, "pachanga@gmail.com", ["Bogota", "Colombia"]];
+const personaDos = ["PACHECO MENDOZA", 23, "pachanga@gmail.com", ["Bogota", "Colombia"]];
+const personaTres = {};
